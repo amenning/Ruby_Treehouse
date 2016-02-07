bd2ac60377c4e3a471f24a8ef5ec723d7c64becb
Andrew Menning <menning23@gmail.com>
2016-02-07 11:39:11 -0500

    Update Name class with attr_accessor

--- RubyObjectsAndClasses/name.rb
+++ RubyObjectsAndClasses/name.rb
@@ -1,5 +1,7 @@
 class Name
-	attr_reader :title, :first_name, :middle_name, :last_name
+	#attr_reader :title, :first_name, :middle_name, :last_name
+	#attr_writer :title
+	attr_accessor :title, :first_name, :middle_name, :last_name
 	
 	def initialize(title, first_name, middle_name, last_name)
 		@title = title
@@ -24,6 +26,11 @@ class Name
 	# def last_name
 		# @last_name
 	# end
+	
+	# def title=(new_title)
+		# @title = new_title
+	# end
+	
 end
 
 name = Name.new("Mr.", "Jason", "", "Seifer")
@@ -31,3 +38,7 @@ puts name.title + " " +
 	 name.first_name + " " +
 	 name.middle_name + " " +
 	 name.last_name
+	 
+puts "Title: #{name.title}"
+name.title = "Dr."
+puts "Title: #{name.title}"
