a8422fb6bd423b4275cb8eaf593c1da6c2c45932
Andrew Menning <menning23@gmail.com>
2016-02-09 17:26:22 -0500

    add instance methods through included module

--- RubyModules/store.rb
+++ RubyModules/store.rb
@@ -1,8 +1,21 @@
 module Inventoryable
 	
+	def stock_count
+		@stock_count ||= 0
+	end
+	
+	def stock_count=(number)
+		@stock_count = number
+	end
+	
+	def in_stock?
+		@stock_count > 0
+	end
+	
 end
 
 class Shirt
+	include Inventoryable
 	attr_accessor :attributes
 	
 	def initialize(attributes)
@@ -29,3 +42,10 @@ end
 shirt1 = Shirt.new(name: "MTF", size: "L")
 shirt2 = Shirt.new(name: "MTF", size: "M")
 
+shirt1.stock_count = 10
+
+puts "Shirt 1 stock count: %s" % shirt1.stock_count
+puts "Shirt 2 stock count: %s" % shirt2.stock_count
+
+puts "Shirt 1 in stock?: %s" % shirt1.in_stock?
+puts "Shirt 2 in stock?: %s" % shirt2.in_stock?
