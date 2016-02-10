00f817cd6b8c6b24b06c2ae40c0347b3a79ac169
Andrew Menning <menning23@gmail.com>
2016-02-09 20:55:19 -0500

    Timezones in Time class

--- RubyCoreAndStandardLibrary/time.rb
+++ RubyCoreAndStandardLibrary/time.rb
@@ -16,3 +16,14 @@ puts time < Time.now
 puts Time.now > time
 puts time.to_a
 puts time.strftime("%b %d %Y")
+
+puts Time.new
+puts time1 = Time.new(2014, 7, 1, 12, 0, 0, "-04:00")
+puts time2 = Time.new(2014, 7, 1, 12, 0, 0, "-05:00")
+puts time2 = Time.new(2014, 7, 1, 11, 0, 0, "-05:00")
+puts time1 == time2
+puts time1.dst?
+puts utc_time1 = time1.dup.utc
+puts utc_time1.utc_offset
+puts time1.utc_offset
+
