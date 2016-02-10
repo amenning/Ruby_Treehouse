217048aaf03c9ae63f01c45f74673d328f5905fb
Andrew Menning <menning23@gmail.com>
2016-02-09 19:01:20 -0500

    struct example

---
+++ RubyCoreAndStandardLibrary/struct.rb
@@ -0,0 +1,21 @@
+# class Customer
+	# attr_accessor :name, :email
+	
+	# def initialize(name, email)
+		# @name, @email = name, email
+	# end
+# end
+
+Customer = Struct.new(:name, :email) do
+	def name_with_email
+		"#{name} <#{email}>"
+	end
+end
+
+customer = Customer.new("Jason", "jason@teamtreehouse.com")
+puts customer.inspect
+puts customer.name_with_email
+
+customer.each_pair do |name, value|
+	puts "#{name} - #{value}"
+end
