b2eab42603f5d5fb6903a6955520c4dc4f36c731
Andrew Menning <menning23@gmail.com>
2016-02-17 23:51:40 -0500

    simplifying tests with factories

--- UserAuthenticationWithRails/odot/spec/factories.rb
+++ UserAuthenticationWithRails/odot/spec/factories.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     sequence(:email) { |n| "user#{n}@odot.com"}
     password "treehouse1"
     password_confirmation "treehouse1"
+  end    
+  
+  factory :todo_list do
+    title "Todo List Title"
+    description "Todo List Description"
+    user
   end
 end
