b404f95a602238cf04215eadcfe8f6e516f70874
Andrew Menning <menning23@gmail.com>
2016-02-20 14:47:11 -0500

    add notifier preview

--- UpgradingARubyOnRailsApplication/odot/config/application.rb
+++ UpgradingARubyOnRailsApplication/odot/config/application.rb
@@ -22,5 +22,6 @@ module Odot
     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/mailer_previews"
   end
 end
