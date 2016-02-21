aa71a138763e881f15a426296cda83a93b97640c
Andrew Menning <menning23@gmail.com>
2016-02-21 11:43:44 -0500

    Remove description from todo lists and fix specs

--- RailsLayoutsAndCSSFrameworks/odot/spec/views/todo_lists/show.html.erb_spec.rb
+++ RailsLayoutsAndCSSFrameworks/odot/spec/views/todo_lists/show.html.erb_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe "todo_lists/show" do
   before(:each) do
     @todo_list = assign(:todo_list, stub_model(TodoList,
-      :title => "Title",
-      :description => "MyText"
+      :title => "Title"
     ))
   end
 
@@ -12,6 +11,5 @@ describe "todo_lists/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     expect(rendered).to match(/Title/)
-    expect(rendered).to match(/MyText/)
   end
 end
