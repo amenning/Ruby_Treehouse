b87f30b8827c86d40f49795452d019cc9b587bee
Andrew Menning <menning23@gmail.com>
2016-02-25 21:07:06 -0500

    initial commit for RailsAssetPipelineAndStyling

---
+++ RailsAssetPipelineAndStyling/odot/app/models/todo_list.rb
@@ -0,0 +1,16 @@
+class TodoList < ActiveRecord::Base
+  has_many :todo_items
+  belongs_to :user
+  
+  validates :title, presence: true
+  validates :title, length: {minimum: 3}
+  
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+  
+  def has_incomplete_items?
+    todo_items.incomplete.size > 0
+  end
+  
+end
